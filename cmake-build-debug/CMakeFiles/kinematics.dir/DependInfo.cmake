a4c78e2375f836afc204b0e2f523e62cdc9e081c
ZixuanHu <47871032+hzxsunshine@users.noreply.github.com>
2020-03-12 17:01:32 -0400

    modified:   src/catmull_rom_interpolation.cpp

---
+++ cmake-build-debug/CMakeFiles/kinematics.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/main.cpp" "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/CMakeFiles/kinematics.dir/main.cpp.o"
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/src/catmull_rom_interpolation.cpp" "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/CMakeFiles/kinematics.dir/src/catmull_rom_interpolation.cpp.o"
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/src/copy_skeleton_at.cpp" "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/CMakeFiles/kinematics.dir/src/copy_skeleton_at.cpp.o"
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/src/end_effectors_objective_and_gradient.cpp" "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/CMakeFiles/kinematics.dir/src/end_effectors_objective_and_gradient.cpp.o"
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/src/euler_angles_to_transform.cpp" "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/CMakeFiles/kinematics.dir/src/euler_angles_to_transform.cpp.o"
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/src/forward_kinematics.cpp" "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/CMakeFiles/kinematics.dir/src/forward_kinematics.cpp.o"
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/src/kinematics_jacobian.cpp" "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/CMakeFiles/kinematics.dir/src/kinematics_jacobian.cpp.o"
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/src/line_search.cpp" "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/CMakeFiles/kinematics.dir/src/line_search.cpp.o"
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/src/linear_blend_skinning.cpp" "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/CMakeFiles/kinematics.dir/src/linear_blend_skinning.cpp.o"
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/src/projected_gradient_descent.cpp" "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/CMakeFiles/kinematics.dir/src/projected_gradient_descent.cpp.o"
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/src/transformed_tips.cpp" "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/CMakeFiles/kinematics.dir/src/transformed_tips.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../json"
+  "../libigl/external/glad/include"
+  "../libigl/external/glfw/include"
+  "../shared/json"
+  "../libigl/cmake/../include"
+  "../libigl/cmake/../external/eigen"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/Users/zixuan/Documents/GitHub/csc2504-A7-kinematics/cmake-build-debug/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
